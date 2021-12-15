5fcfe0363c609280b4edf8858802f0e790126343
Conrad Dean <conrad.p.dean@gmail.com>
2021-12-15 08:06:05 -0500

    add script doc

--- generate_today_scaffold.sh
+++ generate_today_scaffold.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+# USAGE: ./generate_today_scaffold.sh [DAY]
+# EXAMPLES
+#    # generate today's
+# ./generate_today_scaffold.sh
+#    # generate dec 9th's
+# ./generate_today_scaffold.sh 9
+
 generate_crate() {
     CRATE_NAME=crates/day$(printf "%02d" $1)
     cargo new $CRATE_NAME --bin
